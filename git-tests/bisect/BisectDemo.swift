781f9cb5e95d864fc1bd82176a72ee8746a7bed7
Bhanu Mehar <bhanu.psm27@gmail.com>
2025-12-18 22:34:18 +1100

    Add small comment after refactor

--- git-tests/bisect/BisectDemo.swift
+++ git-tests/bisect/BisectDemo.swift
@@ -1,8 +1,10 @@
 import Foundation
 
 func totalScore(_ scores: [Int]) -> Int {
-    return scores.reduce(0) { -e -  }
+    return scores.reduce(0) { $0 - $1 }   // intentionally wrong
 }
+
 
 let scores = [5, 4, 3, 2, 1]
 print("Total score:", totalScore(scores))
+// minor comment
